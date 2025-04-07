2a13e6bd01eb264e3f32be95f43b56dbd133875b
qwk044 <mr.erko04@gmail.com>
2025-04-07 16:45:36 +0500

    измененный MainActivity.kt

--- app/src/main/java/com/example/lab66/MainActivity.kt
+++ app/src/main/java/com/example/lab66/MainActivity.kt
@@ -1,20 +1,16 @@
 package com.example.lab66
-
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = CountryAdapter(getListData())
     }
-
     private fun getListData(): List<Country> {
         return listOf(
             Country("Vietnam (Asia)", "vietnam", 98000000),
